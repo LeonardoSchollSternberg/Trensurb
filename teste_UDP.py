ae54e8be4baa25671cf54c3202293204d9f938ae
Leonardo Sternberg <leossberg@gmail.com>
2020-01-14 16:40:23 -0300

    update funfando lat e lon

--- teste_UDP.py
+++ teste_UDP.py
@@ -1,4 +1,4 @@
-import time # não usado - delay, pegar tempo real
+# import time # não usado - delay, pegar tempo real
 import socket   # soquete
 import mysql.connector  #conexão banco de dados
 
@@ -10,30 +10,28 @@ sock = socket.socket(socket.AF_INET, # Internet
 sock.bind((UDP_IP, UDP_PORT))
 
 # dados da conexão com o servidor
-mydb = mysql.connector.connect(host="localhost",user="root",password="password",auth_plugin='mysql_native_password',database="dadosS200")
+mydb = mysql.connector.connect(host="localhost",user="root",password="password",auth_plugin='mysql_native_password',database="Giraffe")
 mycursor = mydb.cursor()
 
 cont = True
-# id_r=0  # ID PRIMARY KEY de cada data (para testes) - implementação antiga
-sqlform = "INSERT INTO teste (message) VALUES (%s)"
+sqlform = "INSERT INTO teste_3 (latitude, longitude) VALUES (%s, %s)"
 
 while cont:
-    data, addr = sock.recvfrom(1024) # buffer size de 1024 bytes
-    print ("received message:", data)
-    if(data == b'apagar'):  #apaga a tabela e cria uma outra
-        mycursor.execute("DROP TABLE teste; ")
-        mycursor.execute("CREATE TABLE teste (id INT PRIMARY KEY AUTO_INCREMENT, criado_em TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3), message VARCHAR(100));")
-        #id_r=0 - implementação antiga
-    elif(data == b'parar'): #para o programa
+    data1, addr1 = sock.recvfrom(1024) # buffer size de 1024 bytes
+    print ("received message:", data1)
+    if(data1 == b'apagar'):  #apaga a tabela e cria uma outra
+        mycursor.execute("DROP TABLE teste_3; ")
+        mycursor.execute("CREATE TABLE teste_3 (id INT PRIMARY KEY AUTO_INCREMENT, criado_em TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3), latitude VARCHAR(100), longitude(100));")
+    elif(data1 == b'parar'): #para o programa
         cont = False
         # break - pode ser usado em vez de cont
     else:   #coloca na tabela a data recebida
-        entrada = [(data), ]
+        data2, addr2 = sock.recvfrom(1024) # buffer size de 1024 bytes
+        entrada = [(data1), (data2)]
         mycursor.execute(sqlform, entrada)
         mydb.commit()
-        #id_r=id_r+1 - implementação antiga
-        mycursor.execute("SELECT * FROM teste") #printa tabela
-        # mycursor.executemany("SELECT * FROM teste")
+        mycursor.execute("SELECT * FROM teste_3") #printa tabela
+        # mycursor.executemany("SELECT * FROM teste_3")
         for tb in mycursor:
             print(tb)
 sock.close()
